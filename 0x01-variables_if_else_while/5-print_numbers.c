099c00ce7572f86c193e8cc6462772dcc7763f50
JohnWilliamKing1 <johnwilliamwalace@gmail.com.com>
2023-10-08 16:23:39 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ * return: always 0 (success)
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+{
+printf("%d", num);
+}
+printf("\n");
+return (0);
+}
