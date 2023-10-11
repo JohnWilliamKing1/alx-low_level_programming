b6b887839951e4ab024547fecd54f67d9ae044c1
JohnWilliamKing1 <johnwilliamwalace@gmail.com.com>
2023-10-11 11:01:52 -0700

    ./9-times_table | cat -e

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ * Return: viod
+ */
+void times_table(void)
+{
+int i;
+int j;
+int product;
+i = 0;
+while (i < 10)
+{
+j = 0;
+while (j < 10)
+{
+product = i * j;
+if (j == 0)
+{
+_putchar(product + '0');
+}
+else if (product < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(product + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+j++;
+}
+_putchar('\n');
+i++;
+}
+}
