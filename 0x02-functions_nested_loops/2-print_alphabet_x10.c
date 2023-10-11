c7a7d76ea6dcf88cab94564c0c8da59ab9cb1e9c
JohnWilliamKing1 <johnwilliamwalace@gmail.com.com>
2023-10-11 09:28:58 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - MAke alphabet x10 times
+ *Return: void
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char c;
+    i = 0;
+while (i < 10)
+{
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+i++;
+}
+}
