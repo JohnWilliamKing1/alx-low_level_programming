260883bb13d6e15d828125d1044d8f6aa474369e
JohnWilliamKing1 <johnwilliamwalace@gmail.com.com>
2023-10-12 14:21:33 -0700

    0. Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+/**
+ * positive_or_negative - calculates positive or negative
+ * largest_number - calculates the largest number
+ * convert_day - converts the day
+ * print_remaining_days
+ */
 #include <stdio.h>
-
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c)
 int convert_day(int month, int day)
