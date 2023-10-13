960844e7750ec63b373bd8ff7676a9be437a469f
JohnWilliamKing1 <johnwilliamwalace@gmail.com.com>
2023-10-12 19:09:39 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,28 +6,18 @@
  */
 int main(void)
 {
-int i;
-for (i = 1; i <= 100; i++)
+for (int i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
-{
 printf("FizzBuzz ");
-}
-else
-if (i % 3 == 0)
-{
+else if (i % 3 == 0)
 printf("Fizz ");
-}
-else
-if (i % 5 == 0)
-{
+else if (i % 5 == 0)
 printf("Buzz ");
-}
 else
-{
 printf("%d ", i);
-}
 }
 printf("\n");
 return (0);
 }
+
