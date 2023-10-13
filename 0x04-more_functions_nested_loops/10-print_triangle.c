acea1e6501fbf32234cd8bf0782537f4eb7091ea
JohnWilliamKing1 <johnwilliamwalace@gmail.com.com>
2023-10-12 19:32:47 -0700

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,15 +8,15 @@ void print_triangle(int size)
 {
 if (size <= 0)
 {
-putchar('\n');
+_putchar('\n');
 return;
 }
 for (int i = 0; i < size; i++)
 {
 for (int j = 0; j < size - i - 1; j++)
-putchar(' ');
+_putchar(' ');
 for (int j = 0; j <= i; j++)
-putchar('#');
-putchar('\n');
+_putchar('#');
+_putchar('\n');
 }
 }
