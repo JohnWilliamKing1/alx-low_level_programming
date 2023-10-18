71d5b1e1ebf2dbf1a75d6a09c19dc95ef8d671d3
JohnWilliamKing1 <johnwilliamwalace@gmail.com.com>
2023-10-18 09:30:26 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,16 +8,10 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-char *dest_end = dest;
-while (*dest_end != '\0')
-{
-dest_end++;
-}
-for (int i = 0; i < n && src[i] != '\0'; i++)
-{
-*dest_end = src[i];
-dest_end++;
-}
-*dest_end = '\0';
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
 return (dest);
 }
