e4aae031cc88d980023b751f22911bc6928e9733
JohnWilliamKing1 <johnwilliamwalace@gmail.com.com>
2023-10-18 12:52:09 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,25 +7,18 @@
 char *cap_string(char *str)
 {
 int i = 0;
-while (str[i])
+char c;
+while ((c = str[i]) != '\0')
 {
-while (!(str[i] >= 'a' && str[i] <= 'z'))
-i++;
-if (str[i - 1] == ' ' ||
-str[i - 1] == '\t' ||
-str[i - 1] == '\n' ||
-str[i - 1] == ',' ||
-str[i - 1] == ';' ||
-str[i - 1] == '.' ||
-str[i - 1] == '!' ||
-str[i - 1] == '?' ||
-str[i - 1] == '"' ||
-str[i - 1] == '(' ||
-str[i - 1] == ')' ||
-str[i - 1] == '{' ||
-str[i - 1] == '}' ||
-i == 0)
+if (i == 0 || (c != ' ' && c != '\t' && c != '\n' && c != ',' &&
+c != ';' && c != '.' && c != '!' && c != '?' && c != '"' &&
+c != '(' && c != ')' && c != '{' && c != '}'))
+{
+if (c >= 'a' && c <= 'z')
+{
 str[i] -= 32;
+}
+}
 i++;
 }
 return (str);
