e1637b2f1bf72b0a814f9b9656a680c922c47720
JohnWilliamKing1 <johnwilliamwalace@gmail.com>
2023-11-03 02:59:06 +0200

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - ..
+ * @b: ...
+ * Return: ..
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		perror("Error: ");
+		exit(98);
+	}
+	return (ptr);
+}
