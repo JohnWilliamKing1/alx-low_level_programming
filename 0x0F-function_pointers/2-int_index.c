d3e9cbbe0b23822322a7ea61009b98eef36b9302
JohnWilliamKing1 <johnwilliamwalace@gmail.com>
2023-11-09 03:43:34 +0200

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - ..
+ * @array: ..
+ * @size: ..
+ * @cmp: ..
+ * Return: ..
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array != NULL && cmp != NULL && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
