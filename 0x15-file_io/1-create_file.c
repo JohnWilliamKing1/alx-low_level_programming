4652f8c4d6c58965d525917d617dcc36c808803a
JohnWilliamKing1 <johnwilliamwalace@gmail.com>
2023-11-28 02:41:39 +0200

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,22 +9,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-ssize_t write_count;
+int fd, w, len = 0;
+
 if (filename == NULL)
-return (-1);
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (fd == -1)
 return (-1);
 if (text_content != NULL)
 {
-write_count = write(fd, text_content, strlen(text_content));
-if (write_count == -1)
-{
-close(fd);
+for (len = 0; text_content[len];)
+len++;
+}
 return (-1);
-}
-}
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+w = write(fd, text_content, len);
+if (fd == -1 || w == -1)
+return (-1);
 close(fd);
 return (1);
 }
