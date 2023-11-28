04987c48161cf90b0fedcfb57e1df4585a961d40
JohnWilliamKing1 <johnwilliamwalace@gmail.com>
2023-11-28 02:29:21 +0200

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * create_file - ..
+ * @filename: ..
+ * @text_content: ..
+ * Return: ..
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+ssize_t write_count;
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+if (text_content != NULL)
+{
+write_count = write(fd, text_content, strlen(text_content));
+if (write_count == -1)
+{
+close(fd);
+return (-1);
+}
+}
+close(fd);
+return (1);
+}
